2fadc8d2965e5d3713219dbe3c08588f7e874a4b
shrys <shreyas770@yahoo.in>
2018-09-19 01:44:13 +0530

    mimicing deployed already contract

---
+++ src/lottery.js
@@ -0,0 +1,78 @@
+import web3 from "./web3";
+
+const address = "0x094b8Da584dEE5B7d63191b7B74dBb53f767CF5A";
+const abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: "manager",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getContractBalance",
+    outputs: [{ name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getPlayers",
+    outputs: [{ name: "", type: "address[]" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [{ name: "", type: "uint256" }],
+    name: "players",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor"
+  }
+];
+
+// address and abi will help make a local copy of the actual contract on the network
+
+export default new web3.eth.Contract(abi, address);
+
+    // |   Component Renders
+    // |
+    // |   componentDidMount called
+    // |
+    // |   'Call' methods on contract
+    // |
+    // |   Set data on 'state'
+    // V
