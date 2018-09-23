225ad8b59bc997666647b5f59a88265c45594568
shrys <shreyas770@yahoo.in>
2018-09-23 14:56:57 +0530

    css changed

--- src/App.js
+++ src/App.js
@@ -45,12 +45,11 @@ class App extends Component {
         value: web3.utils.toWei(this.state.value, "ether")
       });
     } catch (ex) {
-      this.setState({ message: 'Error: ' + ex.message});
+      this.setState({ message: "Error: " + ex.message });
       console.log(ex);
       isSuccess = false;
     }
-    if (isSuccess)
-      this.setState({ message: "You have been entered!" });
+    if (isSuccess) this.setState({ message: "You have been entered!" });
   };
 
   onClick = async () => {
@@ -63,12 +62,11 @@ class App extends Component {
         from: accounts[0]
       });
     } catch (ex) {
-      this.setState({ message: 'Error: ' + ex.message});
+      this.setState({ message: "Error: " + ex.message });
       console.log(ex);
       isSuccess = false;
     }
-    if (isSuccess)
-      this.setState({ message: "Winner picked!" });
+    if (isSuccess) this.setState({ message: "Winner picked!" });
   };
 
   notify = () => {
@@ -100,35 +98,65 @@ class App extends Component {
             </div>
           </div>
         </div>
-        <div className="row">
+        <div className="row mt-5">
           <div className="col-md-12 text-center">
-            <h1 className="display3 mt-3 mb-0">EtherRaffle</h1>
+            <h1 className="display3 mb-0">EtherRaffle</h1>
             <p className="text-muted mt-0 mb-5">
               Ethereum based Lottery smart contract
             </p>
           </div>
         </div>
-        <div className="row" />
-        <p>This contract is managed by {this.state.manager}</p>
-        <p>
-          There are currently {this.state.players.length} people entered,
-          competing to win {web3.utils.fromWei(this.state.balance, "ether")}
-        </p>
-        <form onSubmit={this.onSubmit}>
-          <h4>Want to try your luck?</h4>
-          <div>
-            <label>Amount of ether to enter</label>
-            <input
-              value={this.state.value}
-              type="number"
-              step=".01"
-              onChange={event => this.setState({ value: event.target.value })}
-            />
+        <div className="card-deck">
+          <div className="card shadow p-3 bg-white rounded col-md-6">
+            <div className="mb-0">
+              <h4>Manager's address</h4>
+              <kbd>{this.state.manager}</kbd>
+              <hr />
+              <div className="container mt-3">
+                <div className="row">
+                  <div className="col col-7 h3 text-right">Total participants</div>
+                  <div className="col h2">
+                    <kbd>{this.state.players.length}</kbd>
+                  </div>
+                  <div className="w-100" />
+                  <div className="col col-7 h3 text-right">
+                    Prize amount <span className="h6 text-muted">ether</span>
+                  </div>
+                  <div className="col h2">
+                    <kbd>{web3.utils.fromWei(this.state.balance, "ether")}</kbd>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <button>Enter</button>
-        </form>
-        <h4>Pick a winner</h4>
-        <button onClick={this.onClick}>Pick</button>
+          <div className="card shadow p-3 bg-white rounded col-md-6">
+            <div className="mb-0">
+              <form onSubmit={this.onSubmit}>
+                <h4>Enter lottery</h4>
+                <div className="input-group ml-2">
+                  <input
+                    type="number"
+                    step="0.01"
+                    className="form-control"
+                    placeholder="amount"
+                    onChange={event =>
+                      this.setState({ value: event.target.value })
+                    }
+                  />
+                  <div className="input-group-append mr-2">
+                    <span className="input-group-text rounded-right">ether</span>
+                  </div>
+                  <button className="btn btn-dark mr-2">Enter</button>
+                </div>
+              </form>
+              <hr />
+              <h4 className="mt-2">Pick a winner</h4>
+              <div className="mr-2">
+                <button className="btn btn-dark btn-block ml-2" onClick={this.onClick}>Pick</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
